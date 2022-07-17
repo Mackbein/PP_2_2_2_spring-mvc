ced444e0b2c0988875fb3b1d8a33b999c2fad112
Andrey Dolgopolov <axweil98@mail.ru>
2022-07-17 18:02:55 +0500

    add CarService

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,34 +1,25 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarService;
 
 @Controller
 public class CarController {
 
+    private final CarService carService;
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping("/cars")
     public String Car(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        List<Car> listCar = new ArrayList<>();
-        listCar.add(new Car("BMV", "218",2018));
-        listCar.add(new Car("Mercedes", "CL",2020));
-        listCar.add(new Car("OPEL", "ASTRA",2017));
-        listCar.add(new Car("MAZDA", "DEMIO",2002));
-        listCar.add(new Car("MAZDA", "3",2013));
-        model.addAttribute("cars", listCar);
-        try {
-            if (count != null) {
-                model.addAttribute("cars", listCar.subList(0, count));
-            }
-        } catch (IndexOutOfBoundsException ex){
-            model.addAttribute("cars", listCar);
-        }
+        model.addAttribute("cars", carService.getListCar(count));
         return "cars";
     }
 }
--- src/main/java/web/models/Car.java
+++ src/main/java/web/models/Car.java
@@ -1,10 +1,27 @@
 package web.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
 
     private String model;
     private String series;
     private int releaseYear;
+
+
+
+
+    private static List<Car> listCar = new ArrayList<>();
+
+    static {
+        listCar.add(new Car("BMV", "218", 2018));
+        listCar.add(new Car("Mercedes", "CL", 2020));
+        listCar.add(new Car("OPEL", "ASTRA", 2017));
+        listCar.add(new Car("MAZDA", "DEMIO", 2002));
+        listCar.add(new Car("MAZDA", "3", 2013));
+    }
+
 
     public Car(String model, String series, int releaseYear) {
         this.model = model;
@@ -34,6 +51,9 @@ public class Car {
 
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
+    }
+    public static List<Car> getListCar() {
+        return listCar;
     }
 
     @Override
